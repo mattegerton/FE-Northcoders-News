d87fac936747b2f17424d728d8e85c0643fe3160
mattegerton <matthewjegerton@googlemail.com>
2018-09-25 11:03:03 +0100

    added hover and styling to nav/main

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -1,36 +1,36 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./css/MainContent.css";
 
 class MainContent extends Component {
   render() {
     return (
       <div>
-        <h3> Main Content</h3>
+        <h3> Welcome to Northcoders News! What would you like to see? </h3>
 
-        <Link to="/topics">
-          <button className="mainContentButton">
+        <NavLink to="/topics" className="mainContentButton">
+          <button>
             <ion-icon name="book" />
             <br />
             Topics
           </button>
-        </Link>
+        </NavLink>
 
-        <Link to="/articles">
-          <button className="mainContentButton">
+        <NavLink to="/articles" className="mainContentButton">
+          <button>
             <ion-icon name="list-box" />
             <br />
             Articles
           </button>
-        </Link>
+        </NavLink>
 
-        <Link to="/users">
-          <button className="mainContentButton">
+        <NavLink to="/users" className="mainContentButton">
+          <button>
             <ion-icon name="contact" />
             <br />
             Users
           </button>
-        </Link>
+        </NavLink>
       </div>
     );
   }
