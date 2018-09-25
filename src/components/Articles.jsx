63058b1b12e3a4957d0458f63868e2d8eb63bedf
mattegerton <matthewjegerton@googlemail.com>
2018-09-25 14:51:43 +0100

    made some further style improvements

--- src/components/Articles.jsx
+++ src/components/Articles.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./css/Articles.css";
 import { Link, Redirect } from "react-router-dom";
 import * as api from "../api";
-import moment from "moment";
+import RenderArticles from "./RenderArticles";
 
 class Articles extends Component {
   state = {
@@ -15,75 +15,21 @@ class Articles extends Component {
     const { articles } = this.state;
     return (
       <div>
-        <h2> Articles </h2>
-        <button>
-          <Link to="/articles/post">Post an article!</Link>
+        <h3> Articles </h3>
+        <button id="articlePostBtn">
+          <Link className="articleButton" to="/articles/post">
+            Post an article!
+          </Link>
         </button>
         {this.state.error.code && (
           <Redirect
             to={{ pathname: "/WrongPath", state: { error: this.state.error } }}
           />
         )}
-        {[...articles]
-          .sort((a, b) => {
-            const c = new Date(a.created_at);
-            const d = new Date(b.created_at);
-            return d - c;
-          })
-          .map(article => {
-            const previewText = `${article.body.slice(0, 200)}...`;
-            return (
-              <div key={article._id}>
-                <table className="articleBlock">
-                  <col width="5%" />
-                  <col width="70%" />
-                  <tbody>
-                    <tr>
-                      <th> {article.votes}</th>
-                      <Link to={`/articles/${article._id}`}>
-                        <th className="articleBody">
-                          <p className="articleTitle"> {article.title} </p>
-                          {previewText}
-                          <p className="articleFooterText">
-                            {`Posted ${moment(article.created_at)
-                              .startOf("second")
-                              .fromNow()} 
-                          by ${article.created_by}`}
-                          </p>
-                        </th>
-                      </Link>
-                      <th>
-                        <Link to={`/articles/${article._id}`}>
-                          <ion-icon name="reorder" />
-                        </Link>
-                      </th>
-                      <th>
-                        <button>
-                          <ion-icon name="thumbs-down" />
-                        </button>
-                      </th>
-                      <th>
-                        <button>
-                          <ion-icon name="thumbs-up" />
-                        </button>
-                      </th>
-                      <th>
-                        <ion-icon name="chatboxes" />
-                        {article.comment_count}
-                      </th>
-                    </tr>
-                  </tbody>
-                </table>
-              </div>
-            );
-          })}
+        <RenderArticles articles={this.state.articles} />
       </div>
     );
   }
-
-  // handleVote = direction => {
-  //   axios.patch("a").then();
-  // };
 
   componentDidMount() {
     this.getArticles(this.props.match.params.topic);
---
+++ src/components/RenderArticles.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import moment from "moment";
+import "./css/RenderArticles.css";
+
+class RenderArticles extends Component {
+  render() {
+    return [...this.props.articles]
+      .sort((a, b) => {
+        const c = new Date(a.created_at);
+        const d = new Date(b.created_at);
+        return d - c;
+      })
+      .map(article => {
+        const previewText = `${article.body.slice(0, 200)}...`;
+        return (
+          <div key={article._id} className="articleWrapper">
+            <Link to={`/articles/${article._id}`} className="articlesLink">
+              <div id="articleText">
+                <p className="articleTitle"> {article.title} </p>
+                <p id="prevText">{previewText}</p>
+                <p className="articleFooterText">
+                  {`Posted ${moment(article.created_at)
+                    .startOf("second")
+                    .fromNow()} 
+                by ${article.created_by}`}
+                </p>
+              </div>
+            </Link>
+            <div className="articleOptions">
+              <button>
+                <Link to={`/articles/${article._id}`}>
+                  <ion-icon name="reorder" />
+                </Link>
+              </button>
+
+              <button>
+                <ion-icon name="thumbs-down" />
+              </button>
+
+              <button>
+                <ion-icon name="thumbs-up" />
+              </button>
+
+              <button>
+                <ion-icon name="chatboxes" />
+                {article.comment_count}
+              </button>
+            </div>
+          </div>
+        );
+      });
+  }
+}
+
+export default RenderArticles;
