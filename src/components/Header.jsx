68eb36e438844a78c9f97b4aa2a5d5626b46a7f4
mattegerton <matthewjegerton@googlemail.com>
2018-09-25 09:43:52 +0100

    header will not return user home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import "./css/Header.css";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="header">
-      <h1> Northcoders News </h1>
+    <div className="header" onClick="">
+      <NavLink to="/" activeClassName="linkStyle">
+        <h1> Northcoders News </h1>
+      </NavLink>
     </div>
   );
 };
