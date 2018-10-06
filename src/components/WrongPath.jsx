dfe2f2472a9714182f3d7fe4a0f39eb431acec40
mattegerton <matthewjegerton@googlemail.com>
2018-10-06 19:18:17 +0100

    add back button to error page

--- src/components/WrongPath.jsx
+++ src/components/WrongPath.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import "./css/WrongPath.css";
+import { Link } from "react-router-dom";
 
 const WrongPath = () => {
   return (
     <div>
-      <ion-icon name="alert" />
-      <p> Error: Path does not exist! </p>
+      <div className="errorAlert">
+        <ion-icon name="alert" />
+        <p> Error: Path does not exist! </p>
+      </div>
+
+      <button id="takeMeBackButton">
+        <Link className="articleButton" to="/articles">
+          Take me back to Articles, please!
+        </Link>
+      </button>
     </div>
   );
 };
