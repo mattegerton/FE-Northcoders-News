d8de6df2e8b3331607efeadcc9011e6e7046a5ad
mattegerton <matthewjegerton@googlemail.com>
2019-01-03 19:41:36 +0000

    fix css for Comments

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import PostComment from "./PostComment";
 import { connect } from "react-redux";
 import { voteOnComment } from "../actions/commentActions";
+import "./css/Comments.css";
 
 class Comments extends Component {
   render() {
@@ -11,73 +12,48 @@ class Comments extends Component {
           <div>
             {this.props.extComments.map(comment => {
               return (
-                <div key={comment._id}>
-                  <table className="commentTable">
-                    <tbody>
-                      <tr>
-                        <th id="commentVoteCount">{comment.votes}</th>
-                        <th id="commentBody">
-                          {comment.body}
-                          <p id="commentCreator">{`Posted by ${
-                            comment.belongs_to
-                          }`}</p>
-                        </th>
-                      </tr>
-                      <tr>
-                        <th>
-                          <button
-                            id={`buttondown${comment._id}`}
-                            // Attempt to make Redux store look up.
-                            // disabled={
-                            //   this.props.commentVoted[comment._id] === "down" &&
-                            //   this.props.comment._id === comment._id
-                            //     ? true
-                            //     : false
-                            // }
-                            disabled={false}
-                            onClick={() =>
-                              this.props.voteOnComment(
-                                comment._id,
-                                "down",
-                                comment,
-                                this.props.commentVoted
-                              )
-                            }
-                          >
-                            <ion-icon name="thumbs-down" />
-                          </button>
-                          <button
-                            id={`buttonup${comment._id}`}
-                            // Attempt to make Redux store look up.
-                            // disabled={
-                            //   this.props.commentVoted[comment._id] === "up"
-                            //     ? true
-                            //     : false
-                            // }
-                            disabled={false}
-                            onClick={() =>
-                              this.props.voteOnComment(
-                                comment._id,
-                                "up",
-                                comment,
-                                this.props.commentVoted
-                              )
-                            }
-                          >
-                            <ion-icon name="thumbs-up" />
-                          </button>
-                        </th>
-                        <th />
-                        <th>
-                          {/* <button
-                            onClick={() => this.deleteComment(comment._id)}
-                          >
-                            <ion-icon name="trash" /> Delete
-                          </button> */}
-                        </th>
-                      </tr>
-                    </tbody>
-                  </table>
+                <div key={comment._id} id="commentContainer">
+                  <div id="commentBodyGroup">
+                    <div id="commentPoster">
+                      {" "}
+                      Posted by: {comment.belongs_to}{" "}
+                    </div>
+                    <div id="commentBody"> {comment.body} </div>
+                  </div>
+                  <div id="commentBtnGroup">
+                    <div id="commentVoteGroup">
+                      <p> Votes </p>
+                      <div> {comment.votes} </div>
+                    </div>
+                    <button
+                      id={`buttondown${comment._id}`}
+                      disabled={false}
+                      onClick={() =>
+                        this.props.voteOnComment(
+                          comment._id,
+                          "down",
+                          comment,
+                          this.props.commentVoted
+                        )
+                      }
+                    >
+                      <ion-icon name="thumbs-down" />
+                    </button>
+                    <button
+                      id={`buttonup${comment._id}`}
+                      disabled={false}
+                      onClick={() =>
+                        this.props.voteOnComment(
+                          comment._id,
+                          "up",
+                          comment,
+                          this.props.commentVoted
+                        )
+                      }
+                    >
+                      <ion-icon name="thumbs-up" />
+                    </button>
+                  </div>
                 </div>
               );
             })}
@@ -88,6 +64,85 @@ class Comments extends Component {
             />
           </div>
         ) : (
+          // <div>
+          //   {this.props.extComments.map(comment => {
+          //     return (
+          //       <div key={comment._id}>
+          //         <table className="commentTable">
+          //           <tbody>
+          //             <tr>
+          //               <th id="commentVoteCount">{comment.votes}</th>
+          //               <th id="commentBody">
+          //                 {comment.body}
+          //                 <p id="commentCreator">{`Posted by ${
+          //                   comment.belongs_to
+          //                 }`}</p>
+          //               </th>
+          //             </tr>
+          //             <tr>
+          //               <th className="voteCommentBtn">
+          // <button
+          //   id={`buttondown${comment._id}`}
+          //   // Attempt to make Redux store look up.
+          //   // disabled={
+          //   //   this.props.commentVoted[comment._id] === "down" &&
+          //   //   this.props.comment._id === comment._id
+          //   //     ? true
+          //   //     : false
+          //   // }
+          //   disabled={false}
+          //   onClick={() =>
+          //     this.props.voteOnComment(
+          //       comment._id,
+          //       "down",
+          //       comment,
+          //       this.props.commentVoted
+          //     )
+          //   }
+          // >
+          //   <ion-icon name="thumbs-down" />
+          // </button>
+          // <button
+          //   id={`buttonup${comment._id}`}
+          //   // Attempt to make Redux store look up.
+          //   // disabled={
+          //   //   this.props.commentVoted[comment._id] === "up"
+          //   //     ? true
+          //   //     : false
+          //   // }
+          //   disabled={false}
+          //   onClick={() =>
+          //     this.props.voteOnComment(
+          //       comment._id,
+          //       "up",
+          //       comment,
+          //       this.props.commentVoted
+          //     )
+          //   }
+          // >
+          //   <ion-icon name="thumbs-up" />
+          // </button>
+          //               </th>
+          //               <th />
+          //               <th>
+          //                 {/* <button
+          //                   onClick={() => this.deleteComment(comment._id)}
+          //                 >
+          //                   <ion-icon name="trash" /> Delete
+          //                 </button> */}
+          //               </th>
+          //             </tr>
+          //           </tbody>
+          //         </table>
+          //       </div>
+          //     );
+          //   })}
+          // <PostComment
+          //   // user={this.props.user}
+          //   articleID={this.props.articleID}
+          //   // commentPosted={this.commentPosted}
+          // />
+          // </div>
           <div>
             <p id="noComment">
               There are no comments yet to show.
